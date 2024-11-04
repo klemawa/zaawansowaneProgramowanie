ff135515b74a467c28c6ea0f55ab770039e4d1e1
klemawa <klementyna.was@edu.uekat.pl>
2024-11-04 14:42:14 +0100

    naprawianie

---
+++ classLibrary.py
@@ -0,0 +1,9 @@
+class Library:
+    def __init__(self, city, street, zipCode, openHours: str, phone):
+        self.city = city
+        self.street = street
+        self.zipCode = zipCode
+        self.openHours = openHours
+        self.phone = phone
+
+
