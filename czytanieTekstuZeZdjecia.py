111709d3176aee3adbe89bd1f27ec136af7db7b6
klemawa <116188369+klemawa@users.noreply.github.com>
2024-12-23 15:02:07 +0100

    usuwanie plikow

--- czytanieTekstuZeZdjecia.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Mentyn\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
-
-image = Image.open('C:\\Users\\Mentyn\\Desktop\\obrazek.jpg')
-text = pytesseract.image_to_string(image)
-print(text)
-
-
